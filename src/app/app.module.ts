b61fc10d8b63b0da1660755a1cc2b56eb99753c4
minorHealthPotion <hereimyourfriend@gmail.com>
2020-10-18 21:11:21 +0300

    2 files modified, 4 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,16 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
 
-import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { AppComponent } from "./app.component";
+import { HelloComponent } from "./hello.component";
+import { CategoriesComponent } from "./categories/categories.component";
+import { DataHandleService } from "./service/data-handle.service";
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule],
+  declarations: [AppComponent, HelloComponent, CategoriesComponent],
+  bootstrap: [AppComponent],
+  providers: [DataHandleService]
 })
-export class AppModule { }
+export class AppModule {}
