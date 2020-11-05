80efe7fafeed4723751fe9cd6a6435668c3ce7e4
minorHealthPotion <hereimyourfriend@gmail.com>
2020-11-05 21:01:50 +0300

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,17 @@ import { AppComponent } from "./app.component";
 import { HelloComponent } from "./hello.component";
 import { CategoriesComponent } from "./categories/categories.component";
 import { DataHandleService } from "./service/data-handle.service";
-import { TasksComponent } from './tasks/tasks.component';
+import { TasksComponent } from "./tasks/tasks.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, HelloComponent, CategoriesComponent, TasksComponent],
+  imports: [BrowserModule, FormsModule, BrowserAnimationsModule],
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    CategoriesComponent,
+    TasksComponent
+  ],
   bootstrap: [AppComponent],
   providers: [DataHandleService]
 })
