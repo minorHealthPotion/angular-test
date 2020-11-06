08a7d90309f477c411dd655f6fca61ae52f32cc6
minorHealthPotion <hereimyourfriend@gmail.com>
2020-11-06 22:36:26 +0300

    2 files modified

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -7,13 +7,32 @@ import { Task } from "../model/Task";
   styleUrls: ["./tasks.component.css"]
 })
 export class TasksComponent implements OnInit {
+  private displayedColumns = string[] = ['color','id', 'title', 'date', 'priority', 'category'];
+  private dataSource: MatTableDataSource<Task>; //контейнер - источник данных для таблицы
+
   tasks: Task[];
   constructor(private dataHandle: DataHandleService) {}
 
   ngOnInit() {
     this.dataHandle.tasksSubject.subscribe(tasks => (this.tasks = tasks));
+    //датасорс обязательно надо создавать для таблицы туда присваевается любой источник бд
+    this.dataSource = new MatTableDataSource(); 
+
+    this.refreshTable();
   }
   toggleTaskCompleted(task: Task) {
     task.completed != task.completed;
   }
+  private getPriorityColor(task: Task):string{
+      if (task.priority && task.priority.color){
+        return task.priority.color;
+      }
+      return  '#fff';
+  }
+
+  private refreshTable(){
+    this.dataSource.data =  this.task;
 }
+}
+
+
